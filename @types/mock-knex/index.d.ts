fd3c6a882e86b3b7fd3766d048bc2c6033bf3420
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-10-23 07:42:23 +0800

    fix: Upgrade to version of knex without security warnings
    BREAKING: Forcing upgrade of knex version

---
+++ @types/mock-knex/index.d.ts
@@ -0,0 +1,16 @@
+declare module 'mock-knex' {
+    import Knex from 'knex'
+
+    export interface QueryDetails {
+        response(value: any): void
+    }
+
+    export function getTracker(): {
+        on: (
+            event: 'query',
+            handler: (query: QueryDetails, step: number) => void,
+        ) => void
+    }
+
+    export function mock(knex: Knex): void
+}
